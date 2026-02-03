47c9c634c5713134bb068fddb323af24e22e4f40
Andrew Lay <guohaolay@gmail.com>
2026-02-02 22:37:06 -0800

    styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,32 @@
-
-import './App.css'
-import { HotBalloon } from './Components/HotBalloon'
-import { RSVPForm } from './Components/RSVPForm'
+import "./App.css";
+// import { HotBalloon } from "./Components/HotBalloon";
+import { RSVPForm } from "./Components/RSVPForm";
 
 function App() {
   return (
     <>
-      <HotBalloon />
-      <h1>Declan's First Flight</h1>
-      <p>Andrew's baby shower</p>
-      <p>Date: 03/21/2026</p>
-      <p>Time: 12pm - 3pm</p>
-      <p>Location: 11209 12th ave NE, Seattle WA 98125</p>
-      <a href="https://www.amazon.com/baby-reg/andrew-lay-may-2026-seattle/27MUHW5NPQ04U">Baby registry</a>
-      <RSVPForm />
+      {/* <HotBalloon /> */}
+      <h1 className="segment">Stork Skyway</h1>
+      <section id="ticket" className="segment">
+        <dl>
+          <dt>Departure date:</dt>
+          <dd>03/21/2026 12:00 PM</dd>
+          <dt>Time:</dt>
+          <dd>12:00 - 15:00</dd>
+          <dt>Destination:</dt>
+          <dd>
+            <a href="https://maps.app.goo.gl/vtwt4mbqKFshrfyG8">
+              11209 12th ave NE, Seattle WA 98125
+            </a>
+          </dd>
+        </dl>
+        <RSVPForm />
+        <a href="https://www.amazon.com/baby-reg/andrew-lay-may-2026-seattle/27MUHW5NPQ04U">
+          Baby registry
+        </a>
+      </section>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/Components/RSVPForm.tsx
+++ src/Components/RSVPForm.tsx
@@ -1,65 +1,91 @@
-import { useState, } from 'react';
-import './RSVPForm.css'
+import { useState } from "react";
+import "./RSVPForm.css";
+import baggage from "../images/baggage.png";
 
 export const RSVPForm = () => {
-    const [attendance, setAttendance] = useState(true);
-    const [isLoading, setIsLoading] = useState(false);
+  const [attendance, setAttendance] = useState(true);
+  const [hasPlusOne, setHasPlusOne] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
-    const handleAttendance = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setAttendance(e.target.value === "yes")
+  const handleAttendance = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setAttendance(e.target.value === "yes");
+  };
+
+  const handlePlusOne = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setHasPlusOne(e.target.checked);
+  };
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setIsLoading(true);
+    const formData = new FormData(e.currentTarget);
+
+    const data = {
+      attending: formData.get("attend") === "yes",
+      hasGuest: formData.get("plus-one") === "1",
+      email: formData.get("email"),
+      name: formData.get("name"),
+    };
+
+    try {
+      await fetch(
+        "https://script.google.com/macros/s/AKfycbx8ipWk9Les5GFHGpzZLS8shzWOnLVs7zFAV6Op1dOVy-2gLB7eFBzpOdFaMPnWhNhl8g/exec",
+        {
+          method: "POST",
+          mode: "no-cors",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(data),
+        },
+      );
+      alert("TODO: RSVP Sent! See you there!");
+      setIsLoading(false);
+    } catch (err) {
+      console.error("Error!", err);
     }
+  };
 
-    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
-        setIsLoading(true);
-        const formData = new FormData(e.currentTarget);
-
-        const data = {
-            attending: formData.get('attend') === "yes",
-            hasGuest: formData.get('plus-one') === '1',
-            email: formData.get('email'),
-            name: formData.get('name'),
-        };
-
-        try {
-            await fetch('https://script.google.com/macros/s/AKfycbx8ipWk9Les5GFHGpzZLS8shzWOnLVs7zFAV6Op1dOVy-2gLB7eFBzpOdFaMPnWhNhl8g/exec', {
-                method: 'POST',
-                mode: 'no-cors',
-                headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify(data)
-            });
-            alert("RSVP Sent! See you there!");
-            setIsLoading(false);
-        } catch (err) {
-            console.error("Error!", err);
-        }
-    }
-
-    return <form onSubmit={handleSubmit} >
-        <label>
-            Full name:
-            <input type="text" name="name" required disabled={isLoading} />
-        </label>
-        <label>
-            <input type="radio" name="attend" value="yes" id="yes" checked={attendance} onChange={handleAttendance} disabled={isLoading} />
-            Can't wait to attend
-        </label>
-        <label>
-            <input type="radio" name="attend" value="no" id="no" checked={!attendance} onChange={handleAttendance} disabled={isLoading} />
-            Will celebrate from afar
-        </label>
-        {attendance &&
-            <>
-                <label>
-                    <input type="checkbox" name="plus-one" value="1" disabled={isLoading} />
-                    I am bringing a plus-one
-                </label>
-                <label>
-                    Email:
-                    <input type="email" name="email" required disabled={isLoading} />
-                </label>
-            </>
-        }
-        <button type="submit" disabled={isLoading}>Submit</button>
+  return (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="name">Passenger:</label>
+      <input type="text" name="name" id="name" required disabled={isLoading} />
+      <label htmlFor="attend">Check-in</label>
+      <select
+        name="attend"
+        id="attend"
+        onChange={handleAttendance}
+        disabled={isLoading}
+      >
+        <option value="yes">Checked In</option>
+        <option value="no">Flight Cancelled</option>
+      </select>
+      {attendance && (
+        <>
+          <label htmlFor="plus-one">Baggage</label>
+          <div id="baggage-counter">
+            <input
+              type="checkbox"
+              name="plus-one"
+              id="plus-one"
+              value="1"
+              disabled={isLoading}
+              onChange={handlePlusOne}
+            />
+            {hasPlusOne ? 1 : 0}/1
+            <img src={baggage} width="50" />
+          </div>
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            required
+            disabled={isLoading}
+          />
+        </>
+      )}
+      <button type="submit" disabled={isLoading}>
+        Submit
+      </button>
     </form>
-}
+  );
+};
